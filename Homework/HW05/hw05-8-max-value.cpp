2e3684084651d013baa616989bfa8b51c6f9f618
jxnipat <Jetnipat.prc@gmail.com>
2023-08-14 02:58:52 +0700

    upppppp

--- Homework/HW05/hw05-8-max-value.cpp
+++ Homework/HW05/hw05-8-max-value.cpp
@@ -37,9 +37,9 @@ int main() {
     printf("3rd Number = %d\n", num3);
 
     if (num1 > num2 && num1 > num3) {
-        printf("The 1st Number is the greatest among three\n");
+        printf("The 1rd Number is the greatest among three\n");
     } else if (num2 > num1 && num2 > num3) {
-        printf("The 2nd Number is the greatest among three\n");
+        printf("The 2rd Number is the greatest among three\n");
     } else {
         printf("The 3rd Number is the greatest among three\n");
     }
